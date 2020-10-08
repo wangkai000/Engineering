3bdfecd12448bf6d166321f0cd44ec355e87bc66
wangkai000 <1071231288@qq.com>
2020-10-09 07:55:05 +0800

    2020/10/9 7:55 ok

--- src/main/java/com/engineering/WangkaiEngineeringApplication.java
+++ src/main/java/com/engineering/WangkaiEngineeringApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 
-//SpringBootApplication 标注类是一个spring boot 应用 启动类下面所有资源被倒入
+//SpringBootApplication 标注类是一个spring boot 应用 启动类下面所有资源被倒入 开启自动配置
 @SpringBootApplication
 public class WangkaiEngineeringApplication {
 
@@ -13,7 +13,7 @@ public class WangkaiEngineeringApplication {
 	public static void main(String[] args) {
 		System.out.println("______start");
 
-		//将springboot 应用启动
+		//将springboot 应用启动 参数1 传入入口类 参数2 mian 函数主题
 		SpringApplication.run(WangkaiEngineeringApplication.class, args);
 	}
 
--- src/main/java/com/engineering/controller/test.java
+++ src/main/java/com/engineering/controller/test.java
@@ -7,13 +7,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+//修饰范围 类 (加入命名空间) 方法(指定具体路径)
 @RequestMapping("test")
 public class test {
 
     @GetMapping("test")
+    //让返回效果在页面上显示
     @ResponseBody
     public  String test(){
-        return  "test测试";
+         return  "test测试";
     }
 
 
